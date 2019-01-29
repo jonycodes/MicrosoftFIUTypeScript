d74b184cd891e902cf569e39740613949732e122
Leon Liang <leli@microsoft.com>
2019-01-29 14:09:14 -0800

    changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,17 +3,45 @@ import './App.css';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
+interface IMyCustomProps {      
+  items: any[],
+  value: ''
+}; 
+class App extends React.Component<{}, IMyCustomProps> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      items: [''],
+      value: ''
+    }
+  }
+
+  public onChange = (event: any) => {
+    this.setState({value: event.target.value});
+  }
+
+  public onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.setState({
+      items: [...this.state.items, this.state.value],
+      value: ''
+    });
+  }
+
   public render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Todo App</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <form className="App" onSubmit={this.onSubmit}>
+            <input value={this.state.value} onChange={this.onChange} />
+            <button>Submit</button>
+        </form>
       </div>
     );
   }
