8ff4ea2711ab7db1dd9e7ad4fdeb0f9abf9e12b7
Joannier Pinales <mr.joannier@yahoo.com>
2019-01-31 17:43:34 -0500

    Create sample.config.ts

---
+++ my-app/src/sample.config.ts
@@ -0,0 +1,7 @@
+export class Config {
+    public static host: string = "";
+    public static configs = "";
+    public static path = "";
+    public static key = "";
+    // back up 32e5d674fcd64c2093c4ef6b14bfa3a5
+}
